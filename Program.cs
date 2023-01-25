62202872507cf2807d8dbf88992f054b7c732030
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-25 16:13:17 +0530

    Main Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace CabInvoiceGeneratorProgram25Jan
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Invoice Generator.");
+
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+    }
+}
